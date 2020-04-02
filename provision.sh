ffad829874dd1dd312eb1c1041426cbdbff1265a
jamienicholls <jamie.n@hotmail.co.nz>
2020-04-03 11:55:41 +1300

    Create provision.sh

---
+++ provision.sh
@@ -0,0 +1,12 @@
+    # Install httpd
+    dnf install httpd -y
+    systemctl enable --now httpd
+    firewall-cmd --permanent --add-service=http
+    
+    # Install MariaDB
+    dnf install mariadb-server mariadb -y
+    systemctl enable --now mariadb
+    
+    # Install php
+    dnf install php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring -y
+    systemctl enable --now php-fpm
