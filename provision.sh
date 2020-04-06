55d2ab7d76db332a12612b024c84dbd92001d756
jamienicholls <jamie.n@hotmail.co.nz>
2020-04-06 17:59:10 +1200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -8,5 +8,5 @@
     systemctl enable --now mariadb
     
     # Install php
-    dnf install php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring -y
+    dnf install php php-fpm php-mysqlnd php-opcache php-gd php-xml php-mbstring php-json -y
     systemctl enable --now php-fpm
